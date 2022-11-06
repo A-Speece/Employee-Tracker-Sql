5eb0a3debaf6e60c140e923dcc3dd4d6912c93e7
Anthony Speece <anthonyspeece@gmail.com>
2022-11-06 00:27:03 -0500

    add mysql to the server.js

---
+++ Develop/db/seeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO department (department_name)
+VALUES ("IT"),
+       ("Engineering"),
+       ("Finance"),
+       ("Legal"),
+       ("Sales");
+
+INSERT INTO roles (title,salary)
+VALUES ("Sales Lead",100000),
+       ("Salesperson",80000),
+       ("Lead Engineer",150000),
+       ("Software Engineer",120000),
+       ("Account Manager",160000),
+       ("Legal Team Lead",250000),
+       ("Lawyer",190000);
+
+INSERT INTO employee (first_name,last_name)
+VALUES ("Anthony","Speece"),
+       ("John","Doe"),
+       ("Jane","Smith");              
